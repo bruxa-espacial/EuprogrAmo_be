15cbc3619611288b501377a8c6f84d87c2c9c03e
Jenifer Angelo <jenmangelo@gmail.com>
2023-05-26 07:33:09 -0300

    so sleepy

--- mulheres.js
+++ mulheres.js
@@ -1,7 +1,7 @@
 const express = require("express")  //iniciandoo express para dar propriedades de servidor para o projeto
-const  { v4: uuidv4 } = require('uuid') //iniciando o uuid para gerar ids unicos para cada mulher
 const router = express.Router() //configurando a primeira parte da rota
 const conectaBancoDeDados = require('./bancoDeDados') //importando o banco de dados
+const Mulher = require('./mulherModel') //importando o modelo de mulher
 
 conectaBancoDeDados() //chamando a função que conecta o banco de dados
 
@@ -12,66 +12,63 @@ app.use(express.json()) //os dados que trafegarem pela requisição serão conve
 
 const porta = 3333 //criando a porta
 
-//criando lista inicial de mulheres
-const mulheres = [
-    {
-        id: '1',
-        nome: 'Simara Conceição',
-        imagem: 'https://github.com/simaraconceicao.png',
-        minibio: 'Desenvolvedora e instrutora'
-    },{
-        id: '2',
-        nome: 'Iana Chan',
-        imagem: 'https://bit.ly/3JCXBqP',
-        minibio: 'CEO & Founder da PrograMaria'
-    },{
-        id: '3',
-        nome: 'Luana Pimentel',
-        imagem: 'https://bit.ly/3FKpFaz',
-        minibio: 'Senior Staff Software Engineer'
-    }
-]
-
 //GET
-function mostraMulheres(request, response){
-   response.json(mulheres)
+async function mostraMulheres(request, response){
+    try {
+        const mulheresVindasDoBancoDeDados = await Mulher.find() //esperando a conexão acontecer e, quando conseguir, busca todas as mulheres no banco de dados
+        //find() guarda o resultado na constante e envia esse objto na resposta da requisição
+        response.json(mulheresVindasDoBancoDeDados)
+    } catch (erro) {
+        console.log(erro)
+    }
 }
 
 //POST
-function criaMulher(request, response){
-    const novaMulher = {
-        id: uuidv4(),
+async function criaMulher(request, response){
+    const novaMulher = new Mulher({
         nome: request.body.nome,
         imagem: request.body.imagem,
+        citacao: request.body.citacao,
         minibio: request.body.minibio
+    })
+
+    try {
+        const mulherCriada = await novaMulher.save() //espera a conexão acontecer e, quando conseguir, salva a nova mulher no banco de dados
+        response.status(201).json(mulherCriada) //envio da resposta (retorna a nova mulher criada)
+    } catch (erro) {
+        console.log(erro)
     }
-    mulheres.push(novaMulher) //acrescenta a nova mulher na lista de mulheres
-    response.json(mulheres) //retorna a lista de mulheres atualizada
+
 }
 
 //PATCH
-function corrigeMulher(request, response){
-    
-    function encontraMulher(mulher){
-        if (mulher.id === request.params.id){ //encontra a mulher pelo ID
-            return mulher
+async function corrigeMulher(request, response){
+
+    try {
+        const mulherEncontrada = await Mulher.findById(request.params.id) //espera a conexão acontecer e, quando conseguir, busca a mulher no banco de dados pelo ID
+
+        // se os dados forem alterados, substitui os dados antigos pelos novos:
+        if (request.body.nome){
+            mulherEncontrada.nome = request.body.nome
         }
+        if (request.body.imagem){
+            mulherEncontrada.imagem = request.body.imagem
+        }
+        if (request.body.citacao){
+            mulherEncontrada.citacao = request.body.citacao
+        }
+        if (request.body.minibio){
+            mulherEncontrada.minibio = request.body.minibio
+        }
+
+        const mulherAtualizadaNoBancoDeDados = await mulherEncontrada.save() //espera a conexão acontecer e, quando conseguir, salva a mulher atualizada no banco de dados
+
+        response.json() //envio da respsta (retorna a lista de mulheres atualizada)
+
+    } catch (erro) {
+        console.log(erro)
     }
 
-    const mulherEncontrada = mulheres.find(encontraMulher) //.find serve para encontrar e retornar dados de num array
-
-    // se os dados forem alterados, substitui os dados antigos pelos novos:
-    if (request.body.nome){
-        mulherEncontrada.nome = request.body.nome
-    }
-    if (request.body.imagem){
-        mulherEncontrada.imagem = request.body.imagem
-    }
-    if (request.body.minibio){
-        mulherEncontrada.minibio = request.body.minibio
-    }
-
-    response.json(mulheres) //envio da respsta (retorna a lista de mulheres atualizada)
 }
 
 //DELETE
